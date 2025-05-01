181dffca57b709e31d6b4d0581802cb2e24c3015
KPRoche <kproche@us.ibm.com>
2025-05-01 06:03:00 -0700

    fixed favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ import { themes as prismThemes } from "prism-react-renderer";
 const config = {
   title: "LLM-d Website",
   tagline: "Powered by Docusaurus",
-  favicon: "img/favicon.ico",
+  favicon: "img/llm-d-favicon.png",
 
   // Set the production url of your site here
   url: "https://your-docusaurus-site.example.com",
