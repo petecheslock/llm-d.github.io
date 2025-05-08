ee551ec9a1529155952548a3f7413fb3e28f9189
Jess Chitas <36636982+jessicachitas@users.noreply.github.com>
2025-05-08 11:18:18 +0100

    change colour palette thoughout website + add new background
    Signed-off-by: Jess Chitas <36636982+jessicachitas@users.noreply.github.com>

--- src/components/Install/index.js
+++ src/components/Install/index.js
@@ -4,7 +4,7 @@ export default function Install() {
     return (
       <>
         {/* ----------------------------- Install Section ---------------------------- */}
-        <div className="install viewport background-2" id="install">
+        <div className="install viewport" id="install">
           <div className="install-info">
           
             <h1 className="install-h1">Installing </h1>
@@ -12,11 +12,11 @@ export default function Install() {
             <h2 className="install-h2">It's as easy as 1...2...llm-d!</h2>
   
             {/* ----------- Modular Section (copy and paste as many as needed) ---------- */}
-            <h3 className="install-h3"><a className="button-link" href="docs/guide/installation/Prerequisites">1. Check the Prerequisites</a></h3>
-            <h3 className="install-h3"><a className="button-link" href="docs/guide/installation/quickstart">2. Run the Quickstart</a></h3>
+            <h3 className="install-h3"><a className="link" href="docs/guide/installation/Prerequisites">1. Check the Prerequisites</a></h3>
+            <h3 className="install-h3"><a className="link" href="docs/guide/installation/quickstart">2. Run the Quickstart</a></h3>
             <h3 className="install-h3">3. Explore llm-d!</h3>
             {/* -------------------------------------------------------------------------- */}
-              <button className="static-button " role="button" href="#">
+              <button className="static-button" role="button" href="#">
                 <a className="button-link" href="docs/guide">
                 Complete installation methods are in the user guide
                 </a>
