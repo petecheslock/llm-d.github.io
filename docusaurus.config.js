73656f56e3031d7d4dcffe031129c31d1e485fd2
Pete Cheslock <pete.cheslock@redhat.com>
2025-06-13 16:27:29 -0400

    Local config
    Signed-off-by: Pete Cheslock <pete.cheslock@redhat.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,18 +25,19 @@ const config = {
   // Set the production url of your site here
   //url: "https://kproche.github.io/",
   // url: "https://jessicachitas.github.io/",
-  url: "https://llm-d.ai/",
+  url: "https://petecheslock.github.io/",
+  // url: "https://llm-d.ai/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  //baseUrl: "/llm-d.github.io/",
-  baseUrl: "/",
+  baseUrl: "/llm-d.github.io/",
+  //baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
 
   //IF YOU ARE DEPLOYING PREVIEW PAGES via GITHUB PAGES FOR THE MAIN REPO
   // UNCOMMENT THE NEXT TWO LINES AND COMMENT OUT THE FORK SECTION BELOW
-  organizationName: "llm-d", // Usually your GitHub org/user name.
+  //organizationName: "llm-d", // Usually your GitHub org/user name.
   //projectName: "webdocs", // Usually your repo name.
 
   //IF YOU ARE DEPLOYING PREVIEW PAGES VIA GITHUB PAGES FOR A BRANCH OF A FORK IN YOUR ACCOUNT
